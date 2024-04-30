9c98a3ef807932559026a61ee93ed4ea5136ddbf
rzatalibov555 <sethubpro@gmail.com>
2024-04-30 19:48:18 +0400

    ternary operator isletmek  (if else)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react'
 import './App.css'
 import './header.css'
 
@@ -25,13 +25,39 @@ function App() {
   }
 
 
-  const Ayxan = () =>{
+  // const Ayxan = () =>{
 
-    // const myInputStyle = {
-    //   width:"200px", 
-    //   height:"50px", 
-    //   color:"green"
-    // }
+  //   // const myInputStyle = {
+  //   //   width:"200px", 
+  //   //   height:"50px", 
+  //   //   color:"green"
+  //   // }
+
+      // return(
+      //   <div className="ayxan_form">
+      //     <label htmlFor="yash">Yashinizi daxil edin</label>
+      //       <input type="text" 
+      //               placeholder='Yawinizi daxil edin'
+      //               id='yash'
+      //               style={{width:"200px", 
+      //               height:"50px", 
+      //               color:"green"}} />
+      //       <button >Send</button>
+      //   </div>
+      // );
+  // }
+  
+
+  class Ayxan extends React.Component{
+    render() {
+
+      const qeydiyyat = true
+      let Text
+      if(qeydiyyat){
+        Text = "Çıxış"
+      }else{
+        Text = "Giriş"
+      }
 
       return(
         <div className="ayxan_form">
@@ -42,10 +68,12 @@ function App() {
                     style={{width:"200px", 
                     height:"50px", 
                     color:"green"}} />
-            <button >Send</button>
+            <button > {Text} </button>
         </div>
       );
+    }
   }
+
 
   return(
     <div className="demo">
